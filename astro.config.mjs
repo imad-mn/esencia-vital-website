f5a0690f7427b9cb96753a730bf01b7d8a8db76f
Imad Marmoud <imadmarmoud@gmail.com>
2026-06-04 16:42:07 -0600

    Commit inicial

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import cloudflare from '@astrojs/cloudflare';
+
+import icon from 'astro-icon';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  adapter: cloudflare(),
+  integrations: [icon()]
+});
